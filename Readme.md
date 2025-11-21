32b215136e91920ba72b40ba4f713f9a617fbaa2
Hugo Reymond <hugo.reymond@insa-lyon.fr>
2025-11-21 17:16:41 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# A minimal minecraft head display server
+
+A server to display a player head : enter the player username, and it will display its head skin.
+
+Save statistics about the queried heads inside flask_minimal.py
+
+# Setup
+
+Requires python
+
+- Install Flask : `pip install flask`
+- Run the server : `flask --app flask_minimal.py`
+
+By default, the server is listening on port 5000
---
+++ flask_minimal.py
@@ -0,0 +1,66 @@
+from flask import Flask
+from flask import request
+import os
+import urllib.request, urllib.error, json
+
+app = Flask(__name__)
+
+# Index page : just display a form asking for minecraft username
+@app.route("/")
+def index():
+    return """
+            <h1></h1> 
+            <form action="/display_skin">
+                <label for="username">Minecraft username:</label><br>
+                <input type="text" id="fname" name="username"><br>
+                <input type="submit">
+            </form>
+            """
+
+# Display skin page : get the player head given it's username, and store the queried names in queried_name.json
+@app.route("/display_skin")
+def display_skin():
+    username = request.args.get("username", "").strip()
+    if not username:
+        return "Missing username", 400
+
+    url = f"https://api.mojang.com/users/profiles/minecraft/{username}"
+    try:
+        with urllib.request.urlopen(url) as resp:
+            if resp.getcode() != 200:
+                return """User not found<a href="/"><input type="button" value="Back"/></a>""", 404
+            data = json.load(resp)
+    except urllib.error.HTTPError as e:
+        if e.code in (204, 404):
+            return """User not found <a href="/"><input type="button" value="Back"/></a>""", 404
+        raise
+
+    player_uuid = data.get("id")
+
+    avatar_url = f"https://mc-heads.net/avatar/{player_uuid}"
+
+    number_of_queries = update_query_names(username)
+    return f"""<h2>Minecraft head of {username}</h2>
+            <img src="{avatar_url}" alt="Minecraft head of {username}">
+            <p>Number of queries for {username} head : {number_of_queries}</p>
+            <a href="/"><input type="button" value="Back"/></a>
+            """
+
+def update_query_names(username) -> int:
+    path = "queried_names.json"
+
+    # Try to retrieve previous stats
+    try:
+        with open(path, "r") as f:
+            stats = json.load(f)
+    except FileNotFoundError, json.JSONDecodeError:
+        stats = {}
+
+    # Update stats for user : username
+    stats[username] = stats.get(username, 0) + 1
+
+    # Save updated stats to "queried_names.json"
+    with open(path, "w") as f:
+        json.dump(stats, f)
+    
+    return stats[username]
